7218ae7be40510e961377b314d94098b0d3b6b3b
Alexio <82485835+ABaconWrappedHotdog@users.noreply.github.com>
2025-01-28 11:16:29 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,9 @@
-public void setup() 
-{
-  String[] lines = {"beast", "dough" ,"happy" ,"question", "star", "three", "eagle", "try"};
-  System.out.println("there are " + lines.length + " lines");
-  for (int i = 0 ; i < lines.length; i++) 
-  {
-    System.out.println(pigLatin(lines[i]));
-  }
-}
-public void draw()
-{
-        //not used
-}
-public int findFirstVowel(String sWord)
-//precondition: sWord is a valid String of length greater than 0.
-//postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-{
-  for(int i = 0; i<sWord.length();i++){
-    if(sWord.substring(i,i+1).equals("a")){
-      return i;
-    }
-    if(sWord.substring(i,i+1).equals("e")){
-      return i;
-    }
-    if(sWord.substring(i,i+1).equals("i")){
-      return i;
-    }
-    if(sWord.substring(i,i+1).equals("o")){
-      return i;
-    }
-    if(sWord.substring(i,i+1).equals("u")){
-      return i;
-    }
-    
-  }
-
-  return -1;
-}
-
-public String pigLatin(String sWord)
+public class Main {
+	
+	public static void main(final String[] args) {
+		PigLatin sim = new PigLatin();
+        	//sim.tester();
+    public String pigLatin(String sWord)
 //precondition: sWord is a valid String of length greater than 0
 //postcondition: returns the pig latin equivalent of sWord
   
@@ -67,3 +33,33 @@ public String pigLatin(String sWord)
     return "ERROR";
   }
 }
+	}
+	public int findFirstVowel(String sWord)
+//precondition: sWord is a valid String of length greater than 0.
+//postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
+{
+  for(int i = 0; i<sWord.length();i++){
+    if(sWord.substring(i,i+1).equals("a")){
+      return i;
+    }
+    if(sWord.substring(i,i+1).equals("e")){
+      return i;
+    }
+    if(sWord.substring(i,i+1).equals("i")){
+      return i;
+    }
+    if(sWord.substring(i,i+1).equals("o")){
+      return i;
+    }
+    if(sWord.substring(i,i+1).equals("u")){
+      return i;
+    }
+    
+  }
+
+  return -1;
+}
+
+}
+
+
